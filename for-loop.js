b5f7af0a26917e6c79ac5a4ddcb894d517586a04
elbowgrrl <76022369+elbowgrrl@users.noreply.github.com>
2021-03-19 14:03:05 -0700

    adding if-statement excercise

---
+++ for-loop.js
@@ -0,0 +1,5 @@
+let total = 0
+let limit = 10
+for (let i = 0; total += i ; i++) {
+console.log(total)
+};
--- if-statement.js
+++ if-statement.js
@@ -1,1 +1,6 @@
-const fruit = 'orange'
+const fruit = "orange" 
+if (fruit.length > 5 ) {
+  console.log("The fruit name has more than five characters.")
+  } else { 
+    console.log("The fruit name has less than five characters.")
+  }
